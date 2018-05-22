8d7e50f6fa90f59c39f6fb864616fc6c69871fe8
andy-dufour <adufour@chef.io>
2018-05-21 23:23:25 -0500

    Adding install script

--- install_a2_monitoring.sh
+++ install_a2_monitoring.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 yum install git -y
+
+curl -LO https://omnitruck.chef.io/install.sh && sudo bash ./install.sh -P chefdk && rm -f install.sh
+
+mkdir /tmp/cookbooks
+
+cd /tmp/cookbooks
+
+git clone "https://github.com/andy-dufour/a2_monitoring.git"
+
+chef-client --local-mode -r "a2_monitoring::default"
